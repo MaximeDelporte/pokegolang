8ca3c954c763d297b69d90b421b17474abf67ab3
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-03-03 21:38:02 +0100

    Setup basic REPL. help and exit commands available.

--- main.go
+++ main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World !")
+	commands := map[string]command{
+		"help": {
+			name: 		 "help",
+			description: "Displays a help message",
+			callback: 	 commandHelp,
+		},
+		"exit": {
+			name: 		 "exit",
+			description: "Exit the Pokedex",
+			callback: 	 commandExit,
+		},
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		if cmd, ok := commands[scanner.Text()]; ok {
+			cmd.callback()
+		} else {
+			fmt.Println("Command doesn't exist.\n")
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
+type command struct {
+	name string
+	description string
+	callback func() error
+}
+
+func commandHelp() error {
+	fmt.Printf("\nWelcome to the Pokedex!\n")
+	fmt.Printf("Usage:\n\n")
+
+	commands := map[string]command{
+		"help": {
+			name: 		 "help",
+			description: "Displays a help message",
+			callback: 	 commandHelp,
+		},
+		"exit": {
+			name: 		 "exit",
+			description: "Exit the Pokedex",
+			callback: 	 commandExit,
+		},
+	}
+
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+
+	fmt.Println("")
+
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
 }
