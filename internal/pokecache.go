06b9916393bf14a8e810a5d607b72d94d12883d4
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-03-05 17:19:50 +0100

    Manage cache inside pokecache. Tests passing.

--- internal/pokecache.go
+++ internal/pokecache.go
@@ -1,6 +1,12 @@
 package internal
 
-import "time"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var mu = &sync.RWMutex{}
 
 type Cache struct {
 	cacheEntry map[string]CacheEntry
@@ -11,14 +17,58 @@ type CacheEntry struct {
 	val       []byte
 }
 
-func (c Cache) Add(key string, value []byte) {
-
+func (c *Cache) Add(key string, value []byte) {
+	mu.Lock()
+	fmt.Println("Create cache for :", key)
+	c.cacheEntry[key] = CacheEntry{
+		createdAt: time.Now(),
+		val:       value,
+	}
+	mu.Unlock()
 }
 
-func (c Cache) Get(key string) ([]byte, bool) {
+func (c *Cache) Get(key string) ([]byte, bool) {
+	mu.RLock()
+	cacheEntry, ok := c.cacheEntry[key]
+	mu.RUnlock()
+
+	if ok {
+		return cacheEntry.val, true
+	}
+
+	fmt.Println("No cache for this entry")
 	return []byte{}, false
 }
 
-func NewCache(interval time.Duration) Cache {
-	return Cache{}
+func NewCache(interval time.Duration) *Cache {
+	fmt.Println("Create cache !")
+	cache := &Cache{
+		cacheEntry: make(map[string]CacheEntry),
+	}
+
+	go cache.reapLoop(interval)
+
+	return cache
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(time.Millisecond)
+	defer ticker.Stop()
+
+	done := make(chan bool)
+	go func() {
+		time.Sleep(interval)
+		done <- true
+	}()
+
+	for {
+		select {
+		case <-done:
+			mu.Lock()
+			fmt.Println("CLEAR CACHE")
+			c.cacheEntry = make(map[string]CacheEntry)
+			mu.Unlock()
+			return
+		}
+	}
 }
